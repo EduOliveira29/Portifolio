280cbf7c37cd6d3625997dcb263096c4a467fece
EduOliveira29 <realmadri123@yahoo.com.br>
2025-03-01 12:16:23 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/teste.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Botão = styled.button`
+  background-color: red;
+`;
+
+function teste () {
+  return <Botão>Clique aqui</Botão>
+}
+
+export default teste;
